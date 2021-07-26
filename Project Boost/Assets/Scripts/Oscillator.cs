ba2498a50f448e414ecb8951d0c7c1a5a4626f23
ssobel25 <ssobel25@gmail.com>
2021-07-26 16:14:48 -0400

    Added Oscillation

---
+++ Project Boost/Assets/Scripts/Oscillator.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Oscillator : MonoBehaviour
+{
+    Vector3 startingPosition;
+    [SerializeField] Vector3 movementVector;
+    float movementFactor;
+
+    [SerializeField] float period = 2f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        startingPosition = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (period <= Mathf.Epsilon) {return;}
+        float cycles = Time.time / period; //constantly growing over time
+        const float tau = Mathf.PI * 2; //constant of ~6.283            
+        float rawSineWave = Mathf.Sin(cycles * tau); //going from -1 to 1
+        movementFactor = (rawSineWave +1f) /2f; //set movement factor from 0 to 1
+        Vector3 offset = movementVector * movementFactor;
+        transform.position = startingPosition + offset;
+    }
+}
